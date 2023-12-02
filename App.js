fd2d5d40ab932d5a56bc6fb4fafdb58dc4d1c02b
Kiran Kamath <Kiran.Kamath@mastercard.com>
2023-12-02 18:41:07 +0530

    episode1 changes

---
+++ App.js
@@ -0,0 +1,20 @@
+/* 
+<div id='parent'>
+   <div id='child'>
+      <h1>I am h1 tag</h1>
+   </div>  
+</div>
+
+*/
+
+const parent = React.createElement('div', { id: 'parent' },
+    [React.createElement('div', { id: 'child' },
+        [React.createElement('h1', {}, 'I am h1 tag'), React.createElement('h1', {}, 'I am another h1 tag')]),
+    React.createElement('div', { id: 'child2' },
+        [React.createElement('h1', {}, 'I am h1 tag'), React.createElement('h1', {}, 'I am another h1 tag')])]);
+
+/* const header = React.createElement('h1', { id: 'header' }, 'Hello from React :>');
+console.log(header); */
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent); 
